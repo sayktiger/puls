667e0941f017132d7d8aad13647c785295b9a673
Konstantin <sayk.tiger09@gmail.com>
2022-03-17 00:01:26 +0300

    adaptive slider

--- src/js/script.js
+++ src/js/script.js
@@ -4,28 +4,28 @@ $(document).ready(function(){
             speed: 1200,
             prevArrow: '<button type="button" class="slick-prev"><img src="img/sliders/shift_left.svg"></button>',
             nextArrow: '<button type="button" class="slick-next"><img src="img/sliders/shift_rigth.svg"></button>',
-            // autoplay: true,
-            // autoplaySpeed: 2000,
-            // responsive: [
-            //     {
-            //       breakpoint: 768,
-            //       settings: {
-            //         dots: true,
-            //         arrows: false,
-            //         centerMode: true,
-            //         centerPadding: '40px'
-            //       }
-            //     },
-            //     {
-            //       breakpoint: 480,
-            //       settings: {
-            //         arrows: false,
-            //         centerMode: true,
-            //         centerPadding: '40px',
-            //         slidesToShow: 1
-            //       }
-            //     }
-            //   ]
+            adaptiveHeight:true,
+            responsive: [
+                {
+                  breakpoint: 768,
+                  settings: {
+                    arrows: false,
+                    dots: true,
+                    centerMode: true,
+                    variableWidth: true,
+                  }
+                },
+                {
+                  breakpoint: 576,
+                  settings: {
+                    arrows: false,
+                    dots: true,
+                    centerMode: true,
+                    slidesToShow: 1,
+                    variableWidth: true,
+                  }
+                }
+              ]
           }
     );
   });
