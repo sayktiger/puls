ac989badc1f596103148358c386c5a8462c1a971
Konstantin <sayk.tiger09@gmail.com>
2022-03-18 15:28:59 +0300

    Modal window

--- src/js/script.js
+++ src/js/script.js
@@ -48,4 +48,21 @@ $(document).ready(function(){
 
     toggleSlide(`.catalog-item__link`);
     toggleSlide(`.catalog-item__back`);
+
+    //Modal window
+    $(`[data-modal=consultation]`).on(`click`, function(){
+      $(`.overlay,#consultation`).fadeIn(`slow`);
+      
+    });
+    $(`.modal__close`).on(`click`, function(){
+      $(`.overlay,#consultation,#order,#thanks`).fadeOut(`slow`);
+    });
+
+
+    $(`.button_mini`).each(function(i){
+      $(this).on(`click`, function(){
+        $(`#order .modal__subtitle`).text($(`.catalog-item__subtitle`).eq(i).text());
+        $(`.overlay,#order`).fadeIn(`slow`);
+      })
+    });
   });
