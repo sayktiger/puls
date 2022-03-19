2323f90984a260e1a6c70cea7de4352cb45207bd
Konstantin <sayk.tiger09@gmail.com>
2022-03-19 11:31:14 +0300

    add php mailer

--- src/js/script.js
+++ src/js/script.js
@@ -98,4 +98,20 @@ $(document).ready(function(){
     validateForms(`#order form`);
 
     $(`input[name="phone"]`).mask("+7(999) 999-9999");
+
+    $(`form`).submit(function(e){
+      e.preventDefault();
+      $.ajax({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize()
+      }).done(function(){
+        $(this).find("input").val("");
+        $(`#consultation, #order`).fadeOut();
+        $(`.overlay, #thanks`).fadeIn();
+
+        $(`form`).trigger(`reset`);
+      });
+      return false;
+    });
   });
