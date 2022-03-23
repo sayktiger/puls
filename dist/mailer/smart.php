0bf41e6707682b3c9f730818762064e1a6879b68
Konstantin <73423824+sayktiger@users.noreply.github.com>
2022-03-23 23:13:11 +0300

    Update smart.php

--- dist/mailer/smart.php
+++ dist/mailer/smart.php
@@ -13,8 +13,8 @@ $mail->CharSet = 'utf-8';
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'sayk.tiger09@gmail.com';                 // Наш логин
-$mail->Password = 'password:sayko003%$%^';                           // Наш пароль от ящика
+$mail->Username = '';                 // Наш логин
+$mail->Password = '';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
